42f4e22e3d5d51885f6d21c6fb9604189f4ab5d8
splendiferousmicrobe <stupendousbacterium@gmail.com>
2026-02-11 14:59:04 -0500

    more opcodes

--- cpu/cpu.cpp
+++ cpu/cpu.cpp
@@ -3,7 +3,7 @@
 CPU::CPU() {
     eax = ebx = ecx = edx = 0;
     esi = edi = 0;
-    ebp = esp = 0;
+    ebp = esp = Memory::SIZE;
     eip = 0;
     flags.ZF = flags.SF = flags.CF = flags.OF = 0;
 }
@@ -61,8 +61,51 @@ void CPU::execute(Memory& mem) {
 
                 flags.ZF = (result == 0);
                 flags.SF = (result & 0x80000000) != 0;
-                flags.CF = val > eax;
+                flags.CF = (val > eax);
                 flags.OF = ((eax ^ result) & (val ^ result) & 0x80000000) != 0;
+
+                eax = result;
+                break;
+            }
+
+            case 0x40: { // INC EAX
+                uint32_t result = eax + 1;
+                flags.ZF = (result == 0);
+                flags.SF = (result & 0x80000000) != 0;
+                flags.OF = (result == 0x80000000) != 0; // max signed positive int + 1
+                // CF not affected
+                eax = result;
+                break;
+            }
+
+            case 0x48: { // DEC EAX
+                uint32_t result = eax - 1;
+                flags.ZF = (result == 0);
+                flags.SF = (result & 0x80000000) != 0;
+                flags.OF = (result == 0x7FFFFFFF) != 0; // max signed positive int - 1
+                eax = result;
+                break;
+            }
+
+            case 0x25: { // AND EAX
+                uint32_t val = fetch32(mem);
+                uint32_t result = eax & val;
+
+                flags.ZF = (result == 0);
+                flags.SF = (result & 0x80000000) != 0;
+                flags.CF = flags.OF = 0;
+
+                eax = result;
+                break;
+            }
+
+            case 0x0D: { // OR EAX
+                uint32_t val = fetch32(mem);
+                uint32_t result = eax | val;
+
+                flags.ZF = (result == 0);
+                flags.SF = (result & 0x80000000) != 0;
+                flags.CF = flags.OF = 0;
 
                 eax = result;
                 break;
@@ -77,6 +120,50 @@ void CPU::execute(Memory& mem) {
                 flags.CF = flags.OF = 0;
 
                 eax = result;
+                break;
+            }
+
+            case 0x3D: { // CMP EAX
+                uint32_t val = fetch32(mem);
+                uint32_t result = eax - val;
+                flags.ZF = (result == 0);
+                flags.SF = (result & 0x80000000) != 0;
+                flags.CF = (val > eax);
+                flags.OF = ((eax ^ val) & (eax ^ result) & 0x80000000) != 0;
+                break;
+            }
+
+            case 0x74: { // JE/JNZ
+                int8_t offset = fetch8(mem); // signed for jumping back
+                if (flags.ZF) {
+                    eip += offset; // offset relative to pointer
+                }
+                break;
+            }
+
+            case 0x75: { // JE/JZ (1 byte immediate)
+                int8_t offset = fetch8(mem);
+                if (!flags.ZF) {
+                    eip += offset;
+                }
+                break;
+            }
+
+            case 0xEB: { // JMP (1 byte immediate)
+                int8_t offset = fetch8(mem);
+                eip += offset;
+                break;
+            }
+
+            case 0x50: { // PUSH EAX
+                esp -= 4; // 4 byte decrease
+                mem.write32(esp, eax); // save eax to esp
+                break;
+            }
+
+            case 0x58: { // POP EAX
+                eax = mem.read32(esp); // load previous eax state back
+                esp += 4; // 4 byte increase
                 break;
             }
 
--- main.cpp
+++ main.cpp
@@ -19,7 +19,36 @@ int main() {
     mem.write8(15, 0x31); // XOR
     mem.write32(16, 0x12345679);
 
-    mem.write8(20, 0xF4); // HLT
+    mem.write8(20, 0x40); // INC
+    mem.write8(21, 0x48); // DEC
+
+    mem.write8(22, 0x3D); // CMP
+    mem.write32(23, 0x02);
+
+    mem.write8(27, 0x74); // JE/JNZ
+    mem.write8(28, 0x01);
+
+    mem.write8(29, 0x40); // INC should not be skipped
+
+    mem.write8(30, 0x75); // JE/JZ
+    mem.write8(31, 0x01);
+
+    mem.write8(32, 0x40); // INC should be skipped
+
+    mem.write8(33, 0xEB); // JMP
+    mem.write8(34, 0x01);
+
+    mem.write8(35, 0x40); // INC should be skipped
+    mem.write8(36, 0x40); // JMP goes here
+
+    mem.write8(37, 0x50); // PUSH 3
+
+    mem.write8(38, 0xB8); // MOV
+    mem.write32(39, 0x00);
+
+    mem.write8(43, 0x58); // POP, eax should be 3 again
+
+    mem.write8(44, 0xF4); // HLT
 
     // run
     cpu.execute(mem);
